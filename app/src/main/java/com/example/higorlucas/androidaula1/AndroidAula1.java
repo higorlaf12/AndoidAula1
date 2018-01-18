1045751b4870a43622fd9906b3076505917dd358
higorlaf12 <higor.freitas@neppoadm.com.br>
2018-01-18 10:32:32 -0200

    Projeto Android inicio

---
+++ app/src/main/java/com/example/higorlucas/androidaula1/AndroidAula1.java
@@ -0,0 +1,13 @@
+package com.example.higorlucas.androidaula1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AndroidAula1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_android_aula1);
+    }
+}
